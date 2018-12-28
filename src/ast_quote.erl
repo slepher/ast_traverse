0cfd8c747188688e8bd791271975041a5ee9f08b
slepher <cxyfree@gmail.com>
2018-12-28 15:35:13 +0800

    update ast_quote line check

--- src/ast_quote.erl
+++ src/ast_quote.erl
@@ -10,7 +10,7 @@
 
 %% API
 -export([parse_transform/2, format_error/1]).
--export([quote/1, quote/2, replace_line/2, merge_clauses/1]).
+-export([abstract/2, quote/1, quote/2, replace_line/2, replace_line_zero/2, merge_clauses/1]).
 
 %%%===================================================================
 %%% API
@@ -24,13 +24,29 @@ format_error(Message) ->
         _    -> io_lib:write(Message)
     end.
 
+abstract(Term, Line) ->
+    erl_syntax:set_pos(erl_syntax:abstract(Term), Line).
+
 replace_line(Ast, Line) ->
+    replace_line_cond(fun(_) -> true end, Ast, Line).
+
+replace_line_zero(Ast, Line) ->
+    replace_line_cond(
+      fun(0) -> true;
+         (_) -> false
+      end, Ast, Line).
+
+replace_line_cond(Cond, Ast, Line) ->
     ast_traverse:map(
       fun(Type, Tuple) when is_tuple(Tuple) and ((Type == pre) or (Type == leaf)) ->
-              TupleList = tuple_to_list(Tuple),
-              case TupleList of
+              case tuple_to_list(Tuple) of
                   [_Action, TupleLine|_Rest] when is_integer(TupleLine) ->
-                      setelement(2, Tuple, Line);
+                      case Cond(TupleLine) of
+                          true ->
+                              setelement(2, Tuple, Line);
+                          false ->
+                              Tuple
+                      end;
                   _ ->
                       Tuple
               end;
@@ -49,39 +65,12 @@ merge_clauses([{'fun', Line, {clauses, _}}|_T] = Nodes) ->
 
 
 quote(Value) ->
-    quote(Value, 0).
+    quote_1(Value, #{line => 0}).
 
-quote({call, _Line1, {atom, _Line2, unquote}, [Unquote]}, _Line) ->
-    Unquote;
-quote([{call, _Line1, {atom, _Line2, unquote_splicing}, [Unquotes]}], _Line) ->
-    Unquotes;
-quote([{atom, _, unquote_splicing}|Unquotes], Line) ->
-    quote_list(Unquotes, Line);
-quote(Tuple, Line) when is_tuple(Tuple) ->
-    TupleList = tuple_to_list(Tuple),
-    QuotedLine = 
-        case TupleList of
-            [_Action, TupleLine|_Rest] when is_integer(TupleLine) ->
-                TupleLine;
-            _ ->
-                Line
-        end,
-    {tuple, QuotedLine, lists:map(fun(Item) -> quote(Item, QuotedLine) end, TupleList)};
-quote([H|T], Line) ->
-    {cons, Line, quote(H, Line), quote(T, Line)};
-quote([], Line) ->
-    {nil, Line};
-quote(Float, Line) when is_float(Float) ->
-    {float, Line, Float};
-quote(Integer, Line) when is_integer(Integer) ->
-    {integer, Line, Integer};
-quote(Atom, Line) when is_atom(Atom) ->
-    {atom, Line, Atom}.
-
-quote_list([H|T], Line) ->
-    {cons, Line, H, quote_list(T, Line)};
-quote_list([], Line) ->
-    {nil, Line}.
+quote(Value, Line) when is_integer(Line) ->
+    quote_1(Value, #{line => Line});
+quote(Value, Opts) when is_map(Opts) ->
+    quote_1(Value, Opts).
 %%--------------------------------------------------------------------
 %% @doc
 %% @spec
@@ -92,9 +81,55 @@ quote_list([], Line) ->
 %%% Internal functions
 %%%===================================================================
 walk(pre, {call, _Line1, {atom, Line2, quote}, [Form]}) ->
-    Node = quote(Form, Line2),
-    Node;
-walk(pre, {call, Line1, {atom, Line2, quote}, [Form, Line]}) ->
-    {call, Line1, {remote, Line2, {atom, Line2, ast_quote}, {atom, Line2, replace_line}}, [quote(Form, Line2), Line]};
+    quote(Form, Line2);
+walk(pre, {call, _Line1, {atom, Line2, quote}, [Form, Line]}) ->
+    {call, Line2, {remote, Line2, {atom, Line2, ast_quote}, {atom, Line2, replace_line_zero}},
+     [quote(Form, #{line => Line2, replaced_line => true}), Line]};
 walk(_Type, Node) ->
     Node.
+
+quote_1({call, _Line1, {atom, _Line2, unquote}, [Unquote]}, _Opts) ->
+    Unquote;
+quote_1([{call, _Line1, {atom, _Line2, unquote_splicing}, [Unquotes]}], _Opts) ->
+    Unquotes;
+quote_1({match, _, {atom, _, unquote}, Unquote}, _Opts) ->
+    Unquote;
+quote_1([{match, _, {atom, _, unquote_splicing}, Unquotes}], _Opts) ->
+    Unquotes;
+quote_1([{atom, _, unquote_splicing}|Unquotes], Opts) ->
+    quote_list(Unquotes, Opts);
+quote_1(Tuple, Opts) when is_tuple(Tuple) ->
+    quote_tuple(Tuple, Opts);
+quote_1([H|T], #{line := Line} = Opts) ->
+    {cons, Line, quote_1(H, Opts), quote_1(T, Opts)};
+quote_1([], #{line := Line}) ->
+    {nil, Line};
+quote_1(Float, #{line := Line}) when is_float(Float) ->
+    {float, Line, Float};
+quote_1(Integer, #{line := Line}) when is_integer(Integer) ->
+    {integer, Line, Integer};
+quote_1(Atom, #{line := Line}) when is_atom(Atom) ->
+    {atom, Line, Atom}.
+
+quote_list([H|T], #{line := Line} = Opts) ->
+    {cons, Line, H, quote_list(T, Opts)};
+quote_list([], #{line := Line}) ->
+    {nil, Line}.
+
+quote_tuple(Tuple, #{line := Line} = Opts) ->
+    TupleList = tuple_to_list(Tuple),
+    case TupleList of
+        [_Action, TupleLine|_Rest] when is_integer(TupleLine) ->
+            NOpts = Opts#{line => TupleLine},
+            {tuple, TupleLine, quote_tuple_list(TupleList, NOpts)};
+        _ ->
+            {tuple, Line, quote_tuple_list_1(TupleList, Opts)}
+    end.
+
+quote_tuple_list([Action, _TupleLine|Rest], #{replaced_line := true} = Opts) ->
+    quote_tuple_list_1([Action, 0|Rest], Opts);
+quote_tuple_list(TupleList, Opts) ->
+    quote_tuple_list_1(TupleList, Opts).
+
+quote_tuple_list_1(List, Opts) ->
+    lists:map(fun(Item) -> quote_1(Item, Opts) end, List).
